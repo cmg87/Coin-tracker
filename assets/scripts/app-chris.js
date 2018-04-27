10443118476bb660221b544061135a38aeb675a5
Chris <chrisgoerler@gmail.com>
2018-04-26 20:02:07 -0400

    added ajax function and pulled data for scrolling ticker

--- assets/scripts/app-chris.js
+++ assets/scripts/app-chris.js
@@ -18,17 +18,29 @@ console.log("Chris's script is connected.");
 //     </div>
 // </div> */}
 
-var queryURL = "https://api.coinmarketcap.com/v1/ticker/"
+var queryURL = "https://api.coinmarketcap.com/v1/ticker/?limit=50";
 
-
+var top50 = [];
+var priceArr = [];
+var symbolArr =[];
+var percentArr = [];
 // Performing our AJAX GET request
 $.ajax({
 url: queryURL,
 method: "GET"
 }).then(function(response){
 console.log(response);
-});
+for(var x in response){
+    top50.push(response[x].name);
+    priceArr.push(response[x].price_usd);
+    percentArr.push(response[x].percent_change_24h);
+    symbolArr.push(response[x].symbol);
 
+}
+console.log(symbolArr);
+console.log(priceArr);
+console.log(percentArr);
+});
 
 function modal(){
 
