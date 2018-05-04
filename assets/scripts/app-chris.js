9c4056f65f223564f90b35015de2983531b27353
Chris <chrisgoerler@gmail.com>
2018-05-03 21:07:42 -0400

    username taken loop fixed

--- assets/scripts/app-chris.js
+++ assets/scripts/app-chris.js
@@ -12,7 +12,7 @@ firebase.initializeApp(config);
 
 var username = firebase.database().ref('user');
 var password = firebase.database().ref('password');
-var returnArr = ["void"];
+var returnArr = [];
 
 // USER LOGIC
 username.on("value", function (snapshot) {
@@ -35,22 +35,29 @@ $("#signup").on("submit", function () {
     event.preventDefault();
     var newuser = $("#username").val().trim();
     var newpassword = $("#password").val().trim();
+    var userTaken = false;
     console.log(newuser);
     console.log(newpassword);
-    for (var x =1; x < returnArr.length; x++) {
+    for (var x = 0; x < returnArr.length; x++) {
+     
         if (newuser == returnArr[x].username) {
-            $('#signup').modal('hide');
-            $('#usertaken').modal('show');
+            userTaken = true;
+            break;
         }
-        else {
-
-            username.push({
-                username: newuser,
-                password: newpassword
-            });
-            $('#signup').modal('hide');
-            location.reload();
-        }
+    }
+   
+    if (!userTaken) {
+        username.push({
+            username: newuser,
+            password: newpassword
+        });
+        $('#signup').modal('hide');
+        $('#login').modal('show');
+        $("#username1").val(newuser);
+        // location.reload();
+    } else {
+        $('#signup').modal('hide');
+        $('#usertaken').modal('show');
     }
 
 
@@ -67,7 +74,7 @@ $("#login").on("submit", function () {
         if (checkuser == returnArr[x].username && checkpassword == returnArr[x].password) {
             window.location.href = "user-tabs.html";
             localStorage.setItem("userarea", checkuser);
-            
+
             // window.open("/home/chris/bootcamp/Project-1/user-tabs.html");
         }
         else {
@@ -75,9 +82,9 @@ $("#login").on("submit", function () {
             $('#wrong').modal('show');
         }
     }
-  
+
 });
-$(window).on("load", function(){
+$(window).on("load", function () {
     $("#userarea").append(localStorage.getItem("userarea"));
     // document.getElementById("userarea").text = localStorage.getItem("name");
 });
@@ -98,99 +105,99 @@ $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
-  // console.log(response);
-  for (var x in response) {
-    top50.push(response[x].name);
-    priceArr.push(response[x].price_usd);
-    percentArr.push(response[x].percent_change_24h);
-    symbolArr.push(response[x].symbol);
-    rankArr.push(response[x].rank);
-    chng1h.push(response[x].percent_change_1h);
-    chng7d.push(response[x].percent_change_7d);
-    chng24h.push(response[x].percent_change_24h);
-  }
-  // console.log("symbolArr:" + symbolArr);
-  // console.log("priceArr:" + priceArr);
-  // console.log("percentARR:" + percentArr);
-  // console.log("top50:" + top50);
+    // console.log(response);
+    for (var x in response) {
+        top50.push(response[x].name);
+        priceArr.push(response[x].price_usd);
+        percentArr.push(response[x].percent_change_24h);
+        symbolArr.push(response[x].symbol);
+        rankArr.push(response[x].rank);
+        chng1h.push(response[x].percent_change_1h);
+        chng7d.push(response[x].percent_change_7d);
+        chng24h.push(response[x].percent_change_24h);
+    }
+    // console.log("symbolArr:" + symbolArr);
+    // console.log("priceArr:" + priceArr);
+    // console.log("percentARR:" + percentArr);
+    // console.log("top50:" + top50);
 
-  // SCROLLING TICKER
-  for (var i = 0; i < top50.length; i++) {
-   
-   var newCoinName = $("<span class= coin-name>")
-   var newCoinInfo = $("<span>")
-   var newCoinPrice = $("<span class = coin-price>")
-   
+    // SCROLLING TICKER
+    for (var i = 0; i < top50.length; i++) {
+
+        var newCoinName = $("<span class= coin-name>")
+        var newCoinInfo = $("<span>")
+        var newCoinPrice = $("<span class = coin-price>")
 
 
-    newCoinName.html(top50[i] + "-- ");
 
-    newCoinInfo.html( "-- " + "ticker: " + symbolArr[i] + "-- " + "% change 24hr: " + percentArr[i] + " | ");
-    newCoinPrice.html( "current price (USD$): " + priceArr[i]);
-    
+        newCoinName.html(top50[i] + "-- ");
+
+        newCoinInfo.html("-- " + "ticker: " + symbolArr[i] + "-- " + "% change 24hr: " + percentArr[i] + " | ");
+        newCoinPrice.html("current price (USD$): " + priceArr[i]);
+
 
         $("#ticker").append(newCoinName)
         $("#ticker").append(newCoinPrice)
         $("#ticker").append(newCoinInfo)
-        
-    
 
-  }
 
-  // CRYPTO RANKINGS TILES AND MODALS
-  for (i = 0; i < top50.length; i++) {
-    modalid = symbolArr[i];
-    // console.log("working");
-    var tile = $("<div>");
-    tile.addClass("col-xs-4 coins");
-    tile.addClass("panel-body");
-    tile.attr("class", "public-tile");
-    tile.html(
-      "<a class='tile-content' href='#' data-toggle='modal' data-target='#" + modalid + "'>" +
-      "<img class='tile-image' src='assets/images/color32/" + symbolArr[i].toLowerCase() + ".png'>" + 
-      "<p class='tile-text'>" + "#" + rankArr[i] + " " + top50[i] + "</p>" + 
-      "</a>"
-    );
-    $("#top50").append(tile);
-    $("span:contains('-')").addClass('red');
-    $("span:contains('-')").addClass('red');
-    $("#cryptomodal").append(
-      "<div class='modal fade' id='" + symbolArr[i] + "' tabindex='-1' role='dialog'>" +
-      "<div class='modal-dialog' role='document'>" +
-      "<div class='modal-content'>" +
-      "<div class='modal-header'>" +
-      "<button type='button' class='close' data-dismiss='modal' aria-label='Close'>" +
-      "<span aria-hidden='true'>&times;</span></button>" +
-      "<h4 class='modal-title'>" +
-      "<img src='assets/images/color32/" + symbolArr[i].toLowerCase() + ".png' class='modalIcon'>" + "#" + rankArr[i] + " - " + top50[i] + "<span class='modal-symbol'>" + symbolArr[i] + "</span></h4>" +
-      "</div>" +
-      "<div class='modal-body'>" +
-      "<h1 class='modal-price'><span>Price:</span><span> " + priceArr[i] + " <span class='modal-currency'>USD</span></span></h1>" +
-      "<table class='table market-table'>" +
-      "<tr>" +
-      "<th>Change: Last Hour</th>" +
-      "<th>Change: 24 Hours</th>" +
-      "<th>Change: 7 Days</th>" +
-      "</tr>" +
-      "<tr>" +
-      "<td>" + "<span class='modal-change-value'>" + chng1h[i] + "</span>" + "</td>" +
-      "<td>" + "<span class='modal-change-value'>" + chng7d[i] + "</span>" + "</td>" +
-      "<td>" + "<span class='modal-change-value'>" + chng24h[i] + "</span>" + "</td>" +
-      "</tr>" +
-      "</table>" +
-      "</div>" +
-      "<div class='modal-footer'>" +
-      "<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>" +
-      "</div>" +
-      "</div>" +
-      "</div>" +
-      "</div>"
-    );
-  }
+
+    }
+
+    // CRYPTO RANKINGS TILES AND MODALS
+    for (i = 0; i < top50.length; i++) {
+        modalid = symbolArr[i];
+        // console.log("working");
+        var tile = $("<div>");
+        tile.addClass("col-xs-4 coins");
+        tile.addClass("panel-body");
+        tile.attr("class", "public-tile");
+        tile.html(
+            "<a class='tile-content' href='#' data-toggle='modal' data-target='#" + modalid + "'>" +
+            "<img class='tile-image' src='assets/images/color32/" + symbolArr[i].toLowerCase() + ".png'>" +
+            "<p class='tile-text'>" + "#" + rankArr[i] + " " + top50[i] + "</p>" +
+            "</a>"
+        );
+        $("#top50").append(tile);
+        $("span:contains('-')").addClass('red');
+        $("span:contains('-')").addClass('red');
+        $("#cryptomodal").append(
+            "<div class='modal fade' id='" + symbolArr[i] + "' tabindex='-1' role='dialog'>" +
+            "<div class='modal-dialog' role='document'>" +
+            "<div class='modal-content'>" +
+            "<div class='modal-header'>" +
+            "<button type='button' class='close' data-dismiss='modal' aria-label='Close'>" +
+            "<span aria-hidden='true'>&times;</span></button>" +
+            "<h4 class='modal-title'>" +
+            "<img src='assets/images/color32/" + symbolArr[i].toLowerCase() + ".png' class='modalIcon'>" + "#" + rankArr[i] + " - " + top50[i] + "<span class='modal-symbol'>" + symbolArr[i] + "</span></h4>" +
+            "</div>" +
+            "<div class='modal-body'>" +
+            "<h1 class='modal-price'><span>Price:</span><span> " + priceArr[i] + " <span class='modal-currency'>USD</span></span></h1>" +
+            "<table class='table market-table'>" +
+            "<tr>" +
+            "<th>Change: Last Hour</th>" +
+            "<th>Change: 24 Hours</th>" +
+            "<th>Change: 7 Days</th>" +
+            "</tr>" +
+            "<tr>" +
+            "<td>" + "<span class='modal-change-value'>" + chng1h[i] + "</span>" + "</td>" +
+            "<td>" + "<span class='modal-change-value'>" + chng7d[i] + "</span>" + "</td>" +
+            "<td>" + "<span class='modal-change-value'>" + chng24h[i] + "</span>" + "</td>" +
+            "</tr>" +
+            "</table>" +
+            "</div>" +
+            "<div class='modal-footer'>" +
+            "<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>" +
+            "</div>" +
+            "</div>" +
+            "</div>" +
+            "</div>"
+        );
+    }
 });
 
 // USER AREA TABS
 $('#myTabs a').on("click", function (i) {
-  i.preventDefault();
-  $(this).tab('show');
+    i.preventDefault();
+    $(this).tab('show');
 });
